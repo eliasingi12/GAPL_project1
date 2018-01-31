9e5e06587ee83de573fd5af8d90099dc53dcf015
eliasingi12 <elias12@ru.is>
2018-01-31 16:16:22 +0000

    Initial

---
+++ GAPL_Project_1/NewGamer.java
@@ -0,0 +1,32 @@
+package GAPL_Project_1;
+
+import java.util.List;
+
+import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class NewGamer extends SampleGamer {
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+
+		StateMachine theMachine = getStateMachine();
+
+		List<Move> moves = theMachine.getLegalMoves(getCurrentState(), getRole());
+		Move move = moves.get(0);
+
+		return move;
+	}
+
+	/*@Override
+	public StateMachine getInitialStateMachine() {
+
+		return OurStateMachine.getInitialStateMachine();
+	}*/
+
+}
