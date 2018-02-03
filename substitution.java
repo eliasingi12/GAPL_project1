09b25b667adc7b150425bc726947f6bfacd89c76
BrynjarSig <brs24@hi.is>
2018-02-03 15:33:36 +0000

    Implemented substitution class

--- substitution.java
+++ substitution.java
@@ -1,11 +1,23 @@
 package GAPL_project1;
 
+import java.util.HashMap;
+
 import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.gdl.grammar.GdlVariable;
 
 public class substitution {
 
-	private Map<GdlVariable, GdlTerm> submap;
+	private HashMap<GdlVariable, GdlTerm> submap;
 
+	public substitution() {
+		submap = new HashMap<GdlVariable, GdlTerm>();
+	}
 
+	public void put(GdlVariable var, GdlTerm term) {
+		submap.put(var, term);
+	}
+
+	public GdlTerm get(GdlVariable var) {
+		return submap.get(var);
+	}
 }
