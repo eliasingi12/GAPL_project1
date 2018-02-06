cae44a12da942521d4eaaeeac726adac502cec92
unknown <peturko@gmail.com>
2018-02-06 19:01:46 +0000

    pseudo code and comments for prover and statemachine

--- OurStateMachine.java
+++ OurStateMachine.java
@@ -26,12 +26,19 @@ public class OurStateMachine extends StateMachine {
 	public int getGoal(MachineState state, Role role) throws GoalDefinitionException {
 		// TODO Auto-generated method stub
 		System.out.print("Role name: " + role.getName() + "\n");
+		// Here we want to return prove([goal(xplayer, V)], rules, {})
+		// and it should return us a set of substitutes
+		//	=> [{V -> ...)}, {V -> ... }, ... }]
 		return 0;
 	}
 
 	@Override
 	public boolean isTerminal(MachineState state) {
 		// TODO Auto-generated method stub
+		// Here we want to return prove([terminal)], rules, {})
+		// and it should return us a set of substitutes
+		// Here we should return random substitutions but not empty []
+		//	=> [{}, {M -> ... }, ... }]
 		return false;
 	}
 
@@ -45,18 +52,27 @@ public class OurStateMachine extends StateMachine {
 	public MachineState getInitialState() {
 		// TODO Auto-generated method stub
 		// return prover.findinitial();
+		// Here we want to return prove([init(F)], rules, {})
+		// and it should return us a set of substitutes
+		//	=> [{F -> cell(1,1,b)}, {F -> cell(1,2,b), ... }]
 		return null;
 	}
 
 	@Override
 	public List<Move> getLegalMoves(MachineState state, Role role) throws MoveDefinitionException {
 		// TODO Auto-generated method stub
+		// Here we want to return prove([legal(xplayer, M)], rules, {})
+		// and it should return us a set of substitutes
+		//	=> [{M -> ...)}, {M -> ... }, ... }]
 		return null;
 	}
 
 	@Override
 	public MachineState getNextState(MachineState state, List<Move> moves) throws TransitionDefinitionException {
 		// TODO Auto-generated method stub
+		// Here we want to return prove([next( ... )], rules, {})
+		// and it should return us a set of substitutes
+		//	=> [{M -> ...)}, {M -> ... }, ... }]
 		return null;
 	}
 
--- Prove.java
+++ Prove.java
@@ -3,9 +3,31 @@ package GAPL_project1;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Prove {
+import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.gdl.grammar.GdlLiteral;
 
+import scala.collection.immutable.List;
+
+public class Prove {
+	// answers should be a new set of items
 	Set<String> answers = new HashSet<String>();
 
+	substitution s = new substitution();
+
+	public Prove(List<GdlLiteral> query, List<Gdl> rules, substitution s) {
+
+		// TODO: If Query is empty, then answers.add(s);
+		// l = query.pop();
+		// TODO: if (l = not p) then if (prove(p,rules,{}) == null) prove(query - l, rules, s)
+		// TODO: if l = distinct(x,y) ...
+		// TODO: else {} // Here l is a instance of GdlSentence
+		// for r in rules do
+		// if r is GdlSentence head = r; body = {}
+		// else ((GdlRule r).getHead(); Body = r.getBody()
+		// s = mgu(l, head, s)
+		// if (s == null) continue
+		// else query2 = query-l + body
+		// 		prove(query2,rules,s)
+	}
 
 }
