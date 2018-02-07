ae11242543325eb8e3f4cfe73be140f1e56df252
BrynjarSig <brs24@hi.is>
2018-02-07 19:47:14 +0000

    Fixing merge conflict

--- UnificationTest.java
+++ UnificationTest.java
@@ -2,47 +2,84 @@ package GAPL_project1;
 
 import org.ggp.base.util.gdl.grammar.GdlFunction;
 import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.gdl.grammar.GdlRelation;
 import org.ggp.base.util.gdl.grammar.GdlTerm;
 import org.ggp.base.util.gdl.grammar.GdlVariable;
 import org.junit.Test;
 
-public class UnitTests {
+public class UnificationTest {
 
 	@Test
 	public void test() {
-		//Junit test = new Junit();
+		substitution mgu; // Most general unifier
+
+		// subtest is the substitution {X->a, Y->V}
 		substitution subtest = new substitution();
+		subtest.put(GdlPool.getVariable("Y"),GdlPool.getVariable("V"));
 		subtest.put(GdlPool.getVariable("X"),GdlPool.getConstant("a"));
-		subtest.put(GdlPool.getVariable("Y"),GdlPool.getConstant("b"));
-		subtest.put(GdlPool.getVariable("V"),GdlPool.getConstant("b"));
 
-		GdlTerm[] terms = new GdlTerm[2];
+		GdlTerm[] terms  = new GdlTerm[1];
+		GdlTerm[] terms2 = new GdlTerm[2];
+		GdlTerm[] terms3 = new GdlTerm[3];
 
-		terms[0] = GdlPool.getVariable("X");
-		terms[1] = GdlPool.getVariable("Y");
-		GdlFunction move1 = GdlPool.getFunction(GdlPool.getConstant("move"), terms);
 
-		terms[0] = GdlPool.getConstant("a");
-		terms[1] = GdlPool.getVariable("V");
-		GdlFunction move2 = GdlPool.getFunction(GdlPool.getConstant("move"), terms);
+		// *****************************
+		// Tests for GdlFunctions
 
-		terms[0] = GdlPool.getConstant("b");
-		terms[1] = GdlPool.getVariable("V");
-		GdlFunction move3 = GdlPool.getFunction(GdlPool.getConstant("move"), terms);
+		// GdlFunction move(X,Y)
+		terms2[0] = GdlPool.getVariable("X");
+		terms2[1] = GdlPool.getVariable("Y");
+		GdlFunction move1 = GdlPool.getFunction(GdlPool.getConstant("move"), terms2);
 
-		substitution empty = new substitution();
-		substitution mgu = OurUnification.mgu(move1,move2,empty);
+		// GdlFunction move(a,V)
+		terms2[0] = GdlPool.getConstant("a");
+		terms2[1] = GdlPool.getVariable("V");
+		GdlFunction move2 = GdlPool.getFunction(GdlPool.getConstant("move"), terms2);
+
+		// GdlFunction move(b,V)
+		terms2[0] = GdlPool.getConstant("b");
+		terms2[1] = GdlPool.getVariable("V");
+		GdlFunction move3 = GdlPool.getFunction(GdlPool.getConstant("move"), terms2);
+
+		// move(X,Y) and move(a,V) should be unifiable
+		mgu = OurUnification.mgu(move1,move2,new substitution());
 		assert subtest.equals(mgu);
-		mgu = OurUnification.mgu(move1,move3,empty);
-		assert !subtest.equals(mgu);
+
+		// move(a,V) and move(b,V) should not be unifiable
+		mgu = OurUnification.mgu(move2,move3,new substitution());
+		assert mgu == null;
+
+
+		// ****************************
+		// Tests for GdlVariables
 
 		GdlVariable Y = GdlPool.getVariable("Y");
 		GdlVariable V = GdlPool.getVariable("V");
 
-		//mgu = Unifier.mgu(Y,V);
-		//assert !subtest.equals(mgu);
+		// mgu for Y and V should be {Y->V} and subtest is {X->a, Y->V}, not equal
+		mgu = OurUnification.mgu(Y,V,new substitution());
+		assert !subtest.equals(mgu);
+		// remove X->a, should be equal now
 		subtest.remove(GdlPool.getVariable("X"));
-		//assert subtest.equals(mgu);
+		assert subtest.equals(mgu);
+
+
+		// ****************************
+		// Tests for GdlRelations
+
+		// next(F) and next(cell(1,2,b)) should have a unifier {F->cell(1,2,b)}
+		terms3[0] = GdlPool.getConstant("1");
+		terms3[1] = GdlPool.getConstant("2");
+		terms3[2] = GdlPool.getConstant("b");
+		GdlFunction cell = GdlPool.getFunction(GdlPool.getConstant("cell"), terms3);
+		terms[0] = cell;
+		GdlRelation rel1 = GdlPool.getRelation(GdlPool.getConstant("next"), terms);
+		terms[0] = GdlPool.getVariable("F");
+		GdlRelation rel2 = GdlPool.getRelation(GdlPool.getConstant("next"), terms);
+
+		mgu = OurUnification.mgu(rel1, rel2, new substitution());
+		System.out.println(mgu);
+		System.out.println(mgu.toString().equals("{F->( cell 1 2 b )}"));
 	}
 
 }
