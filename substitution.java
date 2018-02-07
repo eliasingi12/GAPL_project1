fe5773cae9854429d986bdbe518200aaa8fd1c6c
BrynjarSig <brs24@hi.is>
2018-02-07 15:37:59 +0000

    Change to toString method

--- substitution.java
+++ substitution.java
@@ -50,11 +50,11 @@ public class substitution {
 
 	@Override
 	public String toString() {
-		String str = "";
+		String str = "{";
 		GdlVariable[] keys = this.getKeySet();
 		for (int i = 0; i < keys.length; i++) {
 			str = str + keys[i].toString() + "->" + this.get(keys[i]).toString()+", ";
 		}
-		return str;
+		return str + "}";
 	}
 }
